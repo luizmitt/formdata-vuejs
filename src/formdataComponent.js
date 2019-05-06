919bcc9e74832b869349c8bcaf8ba70dde905810
Luiz Schmitt <lzschmitt@gmail.com>
2019-05-06 16:53:27 -0400

    melhorias no formdata

--- src/formdataComponent.js
+++ src/formdataComponent.js
@@ -1,54 +1,77 @@
+/**
+ * Author Luiz Schmitt <lzschmitt@gmail.com>
+ * 
+ * Criador de formulários dinamicos.
+ * 
+ */
+
 Vue.component('formdata', {
     props: ['urlApi', 'fields', 'actions'],
     template: `
-  <form @submit.prevent="save()">
-    <template v-for="(item,index) in displayedFields">
-      <template v-if="item.input">
-        <div class="form-group">
-          <label :class="[{'formdata-required':item.input.required}]" v-show="item.label" :for="item.field">{{item.label}}</label>
-          <input :type="item.input.type" class="form-control" :class="item.input.class" :id="item.field" :placeholder="item.input.placeholder" :disabled="item.input.disabled" :required="item.input.required" :pattern="item.input.pattern">
+<form class="needs-validation" novalidate @submit.prevent="save()">
+    <template v-for="(row, index) in displayedFields">
+        <div class="row">
+            <div v-for="(column, index) in row" :class="setColumnWidthByRow(row)">
+                <template v-if="column.input">
+                    <div class="form-group">
+                        <label :class="[{'formdata-required':column.input.required}]" v-show="column.label" :for="column.field">{{column.label}}</label>
+                        <input :type="column.input.type" class="form-control" :class="column.input.class" :id="column.field" :placeholder="column.input.placeholder" :disabled="column.input.disabled" :required="column.input.required" :pattern="column.input.pattern">
+                        <div class="invalid-feedback">
+                            Preencha o campo obrigatório.
+                        </div>
+                    </div>
+                </template>
+                <template v-else-if="column.select">
+                    <div class="form-group">
+                        <label v-show="column.label" :for="column.field">{{column.label}}</label>
+                        <select class="form-control" :class="column.select.class" :id="column.field"  :disabled="column.select.disabled" :required="column.select.required">
+                            <option v-show="column.select.placeholder" value>{{column.select.placeholder}}</option>
+                            <option :selected="column.select.selected == data.value" v-for="data in column.select.data" :value="data.value">{{data.text}}</option>
+                        </select>
+                        <div class="invalid-feedback">
+                            Preencha o campo obrigatório.
+                        </div>                        
+                    </div>
+                </template>
+                <template v-else-if="column.checkbox">
+                    <div class="form-check">
+                    <input class="form-check-input" type="checkbox" value="" :id="column.field">
+                    <label class="form-check-label" :for="column.field">
+                        {{column.label}}
+                    </label>
+                    </div>
+                </template>
+                <template v-else-if="column.textarea">
+                    <div class="form-group">
+                        <label :for="column.field">{{column.label}}</label>
+                        <textarea class="form-control" :id="column.field" rows="3"></textarea>
+                    </div>                
+                </template>
+            </div>
         </div>
-      </template>
-      <template v-else-if="item.select">
-        <div class="form-group">
-          <label v-show="item.label" :for="item.field">{{item.label}}</label>
-          <select class="form-control" :class="item.select.class" :id="item.field"  :disabled="item.select.disabled" :required="item.select.required">
-            <option v-show="item.select.placeholder" value>{{item.select.placeholder}}</option>
-            <option :selected="item.select.selected == data.value" v-for="data in item.select.data" :value="data.value">{{data.text}}</option>
-          </select>
-        </div>
-      </template>
-      <template v-else-if="item.checkbox">
-        <div class="form-check">
-          <input class="form-check-input" type="checkbox" value="" :id="item.field">
-          <label class="form-check-label" :for="item.field">
-            {{item.label}}
-          </label>
-        </div>
-      </template>
-      <template v-else-if="item.textarea">
-      </template>
     </template>
     
+    <slot v-bind:formdata="this" />
+
     <template v-if="formdata.actions.length">
-    
+
     </template>
-    <tempalte v-else>
+    <template v-else>
 
     <button type="submit">Salvar</button>
     </template>
-    <slot :formdata="this">
-  </form>
-  `,
+</form>`,
     data() {
         return {
+            // definições atuais do formdata
             formdata: {
                 urlApi: '',
                 fields: this.fields,
                 actions: []
             },
-
+            // definições padrões do formdata
             formdata_default: {
+                rowOnly: false,
                 input: {
                     type: 'text',
                     value: '',
@@ -79,25 +102,65 @@ Vue.component('formdata', {
         }
     },
     computed: {
+        // método para tratar os campos passados no componente para montar o formdata.
         displayedFields() {
+            // hack para utilizar vue dentro de outro scopo
             let self = this;
-            this.formdata.fields.map((item, index) => {
-                if (item.input || item.select || item.checkbox) {
+            // dados dos campos para o formdata
+            let fields = this.formdata.fields;
+            // se não for criado array para definição de linhas/colunas
+            // agrupa todos e faz somente quebra de linhas.
+            if (!Array.isArray(fields[0])) {
+                fields = [];
+                fields.push(this.formdata.fields);
+                this.formdata_default.rowOnly = true;
+            }
+            // verifica se a coluna é um tipo definido, caso contrario,
+            // a coluna é definida como input[type=text] por padrão.
+            fields.map((column, index) => {
+                if (column.input || column.select || column.checkbox) {
                     return false;
                 }
 
-                this.formdata.fields[index] = {
-                    ...item,
+                fields[index] = {
+                    ...column,
                     input: self.formdata_default.input
                 };
             })
-
-            return this.formdata.fields;
+            return fields;
         }
     },
     methods: {
         save() {
 
+            $("input[required]").each(function () {
+                if (!$(this).val().length) {
+                    $(this).addClass('is-invalid');
+                } else {
+                    $(this).removeClass('is-invalid').addClass('is-valid');
+                }
+            });
+
+        },
+
+        // verifica quantas colunas tem por linha e quebra em até 12 colunas.
+        // é necessário fazer uns ajustes de calculos, pro básico já funciona bem.
+        setColumnWidthByRow(row) {
+            let maxColumn = 'col-12';
+
+            if (this.formdata_default.rowOnly) {
+                return maxColumn;
+            }
+
+            let width = parseInt(12 / (Object.keys(row).length - 1));
+
+            if (width > 1 && width < 12) {
+                maxColumn = `col-${Math.ceil(width)}`;
+            } else {
+                maxColumn = 'col-12';
+            }
+
+            return maxColumn;
         }
     }
 });
